1c906b1a88d1dfc99f5af09d0bbefb6442782acb
Niz123 <nizan.naor11@gmail.com>
2025-06-01 20:11:13 +0300

    Configure API base URL using environment variable

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: VITE_BASE_SERVER_URL,
   headers: {
     "Content-Type": "application/json",
   },
